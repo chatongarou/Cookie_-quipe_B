1733cc640a5633eb7089b424c074c176b27e9797
chatongarou <84314532+chatongarou@users.noreply.github.com>
2025-11-04 23:22:51 +0100

    start
    start coockie

---
+++ php/php/login.php
@@ -0,0 +1,22 @@
+<?php
+include 'db.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
+    if (password_verify($password, $user['password'])) {
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        echo "Connexion réussie";
+    } else {
+        echo "Mot de passe incorrect";
+    }
+} else {
+    echo "Utilisateur introuvable";
+}
+?>
---
+++ php/php/register.php
@@ -0,0 +1,13 @@
+<?php
+include 'db.php';
+
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+if ($conn->query($sql) === TRUE) {
+    echo "Inscription réussie";
+} else {
+    echo "Erreur : " . $conn->error;
+}
+?>
