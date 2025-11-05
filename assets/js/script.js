1230cf27dadaeb7f1dfc6ac4cd3f80c538790903
croger-byte <constan.roger@gmail.com>
2025-11-05 21:04:31 +0100

    Implementation animation cookie

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ cookie.addEventListener("click", () => {
   score++;
   scoreDisplay.textContent = score;
 });
+
+cookie.addEventListener('click', () => {
+  // Si la classe existe déjà, on la retire d’abord pour pouvoir relancer l’animation
+  cookie.classList.remove('clicked');
+
+  // Petit délai pour forcer le navigateur à redéclencher l’animation
+  void cookie.offsetWidth;
+
+  cookie.classList.add('clicked');
+});
