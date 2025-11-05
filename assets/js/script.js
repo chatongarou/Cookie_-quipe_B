a2ce9c4eb3f033b1981e399a3ff58316cf096f1a
croger-byte <constan.roger@gmail.com>
2025-11-05 22:06:21 +0100

    implementation autoclicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 let score = 0;
+let NbAutoClicker=0;
 const cookie = document.getElementById("cookie");
 const scoreDisplay = document.getElementById("scoreDisplay");
+const autoClicker_button=document.getElementById("buy_AutoClicker");
+const autoClickerDisplay=document.getElementById("autoClickerDSP")
+
+
+
 
 cookie.addEventListener("click", () => {
   score++;
-  scoreDisplay.textContent = score;
+  scoreDisplay.textContent = score.toFixed(0);
+});
+
+autoClicker_button.addEventListener("click", () => {
+  score-=prix_autoclicker(NbAutoClicker);
+  NbAutoClicker++;
+  autoClickerDisplay.textContent = NbAutoClicker;
+  updateButton();
+  updateScore();
+  update_prix_autoclicker();
 });
 
 cookie.addEventListener('click', () => {
@@ -16,3 +31,33 @@ cookie.addEventListener('click', () => {
 
   cookie.classList.add('clicked');
 });
+
+function updateScore() {
+  // Ajoute 0.5 cookie par autoclicker chaque seconde
+  score += 0.5 * NbAutoClicker;
+  scoreDisplay.textContent = score.toFixed(0); // .toFixed(1) pour 1 décimale
+}
+
+function prix_autoclicker(n){
+  //donne le prix pour acheter 1 autoclicker sachant qu'on en a deja n
+  return 1.5**n
+}
+
+function updateButton() {
+  if (score < prix_autoclicker(NbAutoClicker)) {
+    autoClicker_button.classList.add('locked');
+    autoClicker_button.disabled = true; // désactive le clic
+  } else {
+    autoClicker_button.classList.remove('locked');
+    autoClicker_button.disabled = false; // réactive le clic
+  }
+}
+
+function update_prix_autoclicker(){
+  let display=document.getElementById("dsp_prix_autoclicker")
+  display.textContent=prix_autoclicker(NbAutoClicker).toFixed(1)
+}
+
+update_prix_autoclicker()
+setInterval(updateButton, 1000);
+setInterval(updateScore, 1000);
